8aed73b95426d0d5e092f39732eaa8b6df76ea51
Mathias Scherer <mathischerer@gmail.com>
2026-04-12 09:09:08 -0300

    Websocket connection using websocket-ts

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { WebsocketBuilder } from 'websocket-ts';
+	import { PUBLIC_SERVER_URL } from '$env/static/public';
+
+	const ws = new WebsocketBuilder(`ws://${PUBLIC_SERVER_URL}`)
+		.onError((i, ev) => {
+			console.log('error');
+		})
+        .onMessage((i, ev) => {
+            console.log(ev.data);
+        })
+		.build();
+    
+    function sendMessage() {
+        ws.send("ping")
+    }
+</script>
+
+<button onclick={sendMessage} class="m-3 cursor-pointer rounded border p-2">Send "ping" message</button>
